3ba8dd7b2ba31a19f35b9cea0c34ce1bce16319c
XDLT <rsavychev@gmail.com>
2026-01-22 15:22:17 +0200

    Test

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -48,6 +48,8 @@ namespace WindowsFormsApp1
             this.Share = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -61,8 +63,9 @@ namespace WindowsFormsApp1
             this.Host.Font = new System.Drawing.Font("Segoe Script", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Host.ForeColor = System.Drawing.Color.Black;
             this.Host.Location = new System.Drawing.Point(-2, 0);
+            this.Host.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Host.Name = "Host";
-            this.Host.Size = new System.Drawing.Size(99, 48);
+            this.Host.Size = new System.Drawing.Size(74, 39);
             this.Host.TabIndex = 0;
             this.Host.Text = "Host:";
             // 
@@ -70,9 +73,10 @@ namespace WindowsFormsApp1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
             this.textBox1.Font = new System.Drawing.Font("Segoe Script", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(103, 48);
+            this.textBox1.Location = new System.Drawing.Point(77, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(60, 36);
+            this.textBox1.Size = new System.Drawing.Size(46, 30);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "?";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
@@ -81,9 +85,10 @@ namespace WindowsFormsApp1
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.textBox2.Font = new System.Drawing.Font("Segoe Script", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(103, 6);
+            this.textBox2.Location = new System.Drawing.Point(77, 5);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 36);
+            this.textBox2.Size = new System.Drawing.Size(76, 30);
             this.textBox2.TabIndex = 5;
             this.textBox2.Text = "127.0.0.1";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
@@ -94,9 +99,10 @@ namespace WindowsFormsApp1
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label1.Font = new System.Drawing.Font("Segoe Script", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(-2, 48);
+            this.label1.Location = new System.Drawing.Point(-2, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 48);
+            this.label1.Size = new System.Drawing.Size(74, 39);
             this.label1.TabIndex = 6;
             this.label1.Text = "Port:";
             this.label1.Click += new System.EventHandler(this.label1_Click_2);
@@ -111,9 +117,10 @@ namespace WindowsFormsApp1
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.Host);
-            this.panel1.Location = new System.Drawing.Point(167, 39);
+            this.panel1.Location = new System.Drawing.Point(125, 32);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(217, 96);
+            this.panel1.Size = new System.Drawing.Size(164, 79);
             this.panel1.TabIndex = 7;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -123,9 +130,10 @@ namespace WindowsFormsApp1
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.button1);
-            this.panel2.Location = new System.Drawing.Point(378, 39);
+            this.panel2.Location = new System.Drawing.Point(284, 32);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(360, 96);
+            this.panel2.Size = new System.Drawing.Size(271, 79);
             this.panel2.TabIndex = 8;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
@@ -135,9 +143,10 @@ namespace WindowsFormsApp1
             this.button2.DialogResult = System.Windows.Forms.DialogResult.No;
             this.button2.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(3, 48);
+            this.button2.Location = new System.Drawing.Point(2, 39);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(348, 36);
+            this.button2.Size = new System.Drawing.Size(261, 29);
             this.button2.TabIndex = 3;
             this.button2.Text = "Stop";
             this.button2.UseVisualStyleBackColor = false;
@@ -149,9 +158,10 @@ namespace WindowsFormsApp1
             this.button1.DialogResult = System.Windows.Forms.DialogResult.No;
             this.button1.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Location = new System.Drawing.Point(2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(348, 36);
+            this.button1.Size = new System.Drawing.Size(261, 29);
             this.button1.TabIndex = 2;
             this.button1.Text = "Share";
             this.button1.UseVisualStyleBackColor = false;
@@ -162,9 +172,10 @@ namespace WindowsFormsApp1
             this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel3.Controls.Add(this.textBox3);
-            this.panel3.Location = new System.Drawing.Point(167, 165);
+            this.panel3.Location = new System.Drawing.Point(125, 134);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(571, 202);
+            this.panel3.Size = new System.Drawing.Size(429, 165);
             this.panel3.TabIndex = 9;
             this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
@@ -172,10 +183,11 @@ namespace WindowsFormsApp1
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.textBox3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.textBox3.Location = new System.Drawing.Point(51, 20);
+            this.textBox3.Location = new System.Drawing.Point(38, 16);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(460, 180);
+            this.textBox3.Size = new System.Drawing.Size(346, 147);
             this.textBox3.TabIndex = 0;
             // 
             // panel4
@@ -185,17 +197,19 @@ namespace WindowsFormsApp1
             this.panel4.Controls.Add(this.Connect);
             this.panel4.Controls.Add(this.Share);
             this.panel4.Location = new System.Drawing.Point(0, 1);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(161, 366);
+            this.panel4.Size = new System.Drawing.Size(122, 298);
             this.panel4.TabIndex = 10;
             // 
             // Connect
             // 
             this.Connect.BackColor = System.Drawing.Color.Transparent;
             this.Connect.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Connect.Location = new System.Drawing.Point(-2, 172);
+            this.Connect.Location = new System.Drawing.Point(-2, 140);
+            this.Connect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Connect.Name = "Connect";
-            this.Connect.Size = new System.Drawing.Size(161, 136);
+            this.Connect.Size = new System.Drawing.Size(121, 110);
             this.Connect.TabIndex = 1;
             this.Connect.Text = "Connect tab";
             this.Connect.UseVisualStyleBackColor = false;
@@ -205,9 +219,10 @@ namespace WindowsFormsApp1
             // 
             this.Share.BackColor = System.Drawing.Color.Transparent;
             this.Share.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Share.Location = new System.Drawing.Point(-2, 30);
+            this.Share.Location = new System.Drawing.Point(-2, 24);
+            this.Share.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Share.Name = "Share";
-            this.Share.Size = new System.Drawing.Size(161, 136);
+            this.Share.Size = new System.Drawing.Size(121, 110);
             this.Share.TabIndex = 0;
             this.Share.Text = "Share tab";
             this.Share.UseVisualStyleBackColor = false;
@@ -218,9 +233,10 @@ namespace WindowsFormsApp1
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(167, 9);
+            this.label2.Location = new System.Drawing.Point(125, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(147, 27);
+            this.label2.Size = new System.Drawing.Size(110, 22);
             this.label2.TabIndex = 11;
             this.label2.Text = "Sharing";
             // 
@@ -229,20 +245,41 @@ namespace WindowsFormsApp1
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(167, 137);
+            this.label3.Location = new System.Drawing.Point(125, 111);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 27);
+            this.label3.Size = new System.Drawing.Size(64, 22);
             this.label3.TabIndex = 12;
             this.label3.Text = "Output";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(393, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Руский";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(474, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "English";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(743, 369);
+            this.ClientSize = new System.Drawing.Size(557, 300);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel4);
@@ -251,6 +288,7 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -284,6 +322,8 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
